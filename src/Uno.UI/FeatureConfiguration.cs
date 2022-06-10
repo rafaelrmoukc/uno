c1c678ea1c3ff891b5c198a1f1d2bed1b70e1828
xiaoy312 <xiaoyao312@gmail.com>
2022-06-10 14:17:44 -0400

    debug code

--- src/Uno.UI/FeatureConfiguration.cs
+++ src/Uno.UI/FeatureConfiguration.cs
@@ -14,6 +14,12 @@ namespace Uno.UI
 {
 	public static class FeatureConfiguration
 	{
+		public static class Asd
+		{
+			public const string SanityTimestamp = "12T18";
+			public static bool EnableFix = false;
+		}
+
 		public static class ApiInformation
 		{
 			/// <summary>
